70149324f6f14fafbe5e8b325ade4a1f19a00003
Wee Seng <wee.bscl@gmail.com>
2023-02-14 09:09:36 +0800

    init commit

---
+++ src/test/java/nus/iss/tfip/pafday27/PafDay27ApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.tfip.pafday27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafDay27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
